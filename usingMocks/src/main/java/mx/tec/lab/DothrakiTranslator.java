71b9ac1f0953136f20303c0e2fc82cd348cdd409
Scyruz <a01561869@itesm.mx>
2021-03-02 17:06:26 -0700

    init

---
+++ usingMocks/src/main/java/mx/tec/lab/DothrakiTranslator.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package mx.tec.lab;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author priscy
+ *
+ */
+public class DothrakiTranslator {
+	
+	Map<String, String> translationMap;
+	
+	public DothrakiTranslator() {
+		translationMap = new HashMap<String, String>();	
+	}
+	
+	public void addTranslation(final String word, final String translation) {
+		translationMap.put(word, translation);
+	}
+	
+	public String getTranslation (final String word) {
+		return translationMap.get(word);
+	}
+
+}
